96b7ceafe858cbfe90108988889537a355c6fde0
0xNotes <notes@sigil.systems>
2022-08-08 00:36:09 -0500

    Split Sigil & Systems

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import logo from "../assets/SigilSystems.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import TEST from "../assets/TEST.svg";
+import Sigil from "../assets/Sigil.svg";
+import SYSTEMS from "../assets/SYSTEMS.svg";
+
 
 
 export default function Home() {
@@ -10,16 +12,19 @@ export default function Home() {
     const [offsetY, setOffsetY] = useState(0);
     const [loaded, setLoaded] = useState(false);
 
-    const handleScroll = (event: any)  => {
+    const handleScroll = (event: any) => {
         console.log('scrollTop: ', event.currentTarget.scrollTop);
         console.log('offsetHeight: ', event.currentTarget.offsetHeight);
-      };
-    
+    };
 
-    return(
+
+    return (
         <div className="Home" onScroll={handleScroll}>
-            <img className="Logo" src={TEST}/>
-            <FontAwesomeIcon className="Arrow" icon={faArrowDown}/>
+            <div className="LogoContainer">
+                <img className="LogoSigil" src={Sigil} />
+                <img className="LogoSystems" src={SYSTEMS} />
+            </div>
+            <FontAwesomeIcon className="Arrow" icon={faArrowDown} />
         </div>
     );
 }

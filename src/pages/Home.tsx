177a0e2e10357c5b9e33dbd08fac7d070a4b8682
0xNotes <notes@sigil.systems>
2022-08-08 03:12:24 -0500

    Re-add parallax

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,20 +9,40 @@ import SYSTEMS from "../assets/SYSTEMS.svg";
 
 export default function Home() {
 
-    const [offsetY, setOffsetY] = useState(0);
-    const [loaded, setLoaded] = useState(false);
+    const [animationPlay, setAnimationPlay] = useState(false);
 
     const handleScroll = (event: any) => {
         console.log('scrollTop: ', event.currentTarget.scrollTop);
         console.log('offsetHeight: ', event.currentTarget.offsetHeight);
     };
 
+    const [offsetY, setOffsetY] = useState(0);
+    const slideSlower = offsetY * 0.1;
+
+    useEffect(() => {
+        function handleScroll() {
+          setOffsetY(window.pageYOffset);
+          console.log(offsetY);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, [offsetY]);
 
     return (
         <div className="Home" onScroll={handleScroll}>
             <div className="LogoContainer">
-                <img className="LogoSigil" src={Sigil} />
-                <img className="LogoSystems" src={SYSTEMS} />
+                {(offsetY !== 0) ? <img className="LogoSlideSigil" style={{transform: `translatex(-${slideSlower}rem)`}}  src={Sigil} />
+                    : <img className="LogoSigil" src={Sigil} />
+                }
+                {/* <img className="LogoSigil" src={Sigil} /> */}
+                {(offsetY !== 0) ? <img className="LogoSlideSystems" style={{transform: `translatex(${slideSlower}rem)`}} src={SYSTEMS} /> :
+                    <img className="LogoSystems" src={SYSTEMS} />
+                }
+                {/* <img className="LogoSystems" src={SYSTEMS} /> */}
             </div>
             <FontAwesomeIcon className="Arrow" icon={faArrowDown} />
         </div>

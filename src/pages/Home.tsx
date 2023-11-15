ee05aa7559b683137a5c5f9b63167ced628effba
notes <notes@kali.kali>
2023-11-14 23:46:59 -0600

    Added deploy scripts, fixxed bugs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,95 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/SigilSystems.svg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import Sigil from "../assets/Sigil.svg";
 import SYSTEMS from "../assets/SYSTEMS.svg";
 
-
-
 export default function Home() {
+  const [animationPlayed, setAnimationPlayed] = useState(false);
 
-    const [animationPlay, setAnimationPlay] = useState(false);
+  const handleScroll = (event: any) => {
+    console.log("scrollTop: ", event.currentTarget.scrollTop);
+    console.log("offsetHeight: ", event.currentTarget.offsetHeight);
+  };
 
-    const handleScroll = (event: any) => {
-        console.log('scrollTop: ', event.currentTarget.scrollTop);
-        console.log('offsetHeight: ', event.currentTarget.offsetHeight);
+  const [offsetY, setOffsetY] = useState(0);
+  const slideSlower = offsetY * 0.1;
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffsetY(window.pageYOffset);
+      console.log(offsetY);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, [offsetY]);
 
-    const [offsetY, setOffsetY] = useState(0);
-    const slideSlower = offsetY * 0.1;
+  useEffect(() => {
+    // Check if the animation has played already
+    if (!animationPlayed && offsetY !== 0) {
+      // Set animationPlayed to true to prevent future animations
+      setAnimationPlayed(true);
+    }
+  }, [animationPlayed, offsetY]);
 
-    useEffect(() => {
-        function handleScroll() {
-          setOffsetY(window.pageYOffset);
-          console.log(offsetY);
-        }
+  return (
+    <div className="home" onScroll={handleScroll}>
+      <div className="logo-container">
+        {animationPlayed ? (
+          <img
+            className="logo-slide-sigil"
+            style={{ transform: `translateX(-${slideSlower}rem)` }}
+            src={Sigil}
+          />
+        ) : (
+          <img className="logo-sigil" src={Sigil} />
+        )}
+        {animationPlayed ? (
+          <img
+            className="logo-slide-systems"
+            style={{ transform: `translateX(${slideSlower}rem)` }}
+            src={SYSTEMS}
+          />
+        ) : (
+          <img className="logo-systems" src={SYSTEMS} />
+        )}
+      </div>
+      <FontAwesomeIcon className="arrow" icon={faArrowDown} />
 
-        window.addEventListener("scroll", handleScroll);
+      <div className="description-container">
+      <section id="portfolio">
+        <h1>Professional Full Stack Engineer</h1>
+        <h2>Web3, Web2, and Legacy System Expertise</h2>
+        <p>Welcome to my portfolio! I am a versatile and seasoned full stack engineer, specializing in a comprehensive range of development services spanning Web3, Web2, and legacy systems. My expertise lies in creating robust, scalable, and innovative solutions tailored to meet the evolving needs of businesses and individuals in the digital age.</p>
+        
+        <section id="skills">
+            <h3>Key Skills and Technologies:</h3>
+            <ul>
+                <li><strong>Front-End Development:</strong> Proficient in React, Angular, and Vue.</li>
+                <li><strong>Back-End Development:</strong> Skilled in JavaScript and TypeScript.</li>
+                <li><strong>Versatile Programming:</strong> Experienced in C#, C++, C, Java, and Python.</li>
+                <li><strong>Web Development:</strong> Specialized in designing and developing functional and SEO-friendly websites.</li>
+                <li><strong>Hosting and Administration:</strong> Expert in managing website hosting and administration.</li>
+                <li><strong>Blockchain Solutions:</strong> Adept in creating smart contracts, developing NFTs, and building APIs.</li>
+                <li><strong>Full Stack Engineering:</strong> Comprehensive handling of development from concept to deployment.</li>
+            </ul>
+        </section>
 
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      }, [offsetY]);
+        <section id="offer">
+            <h3>What I Offer:</h3>
+            <p>Customized solutions, quality and reliability, and end-to-end services for a hassle-free development experience.</p>
+        </section>
 
-    return (
-        <div className="Home" onScroll={handleScroll}>
-            <div className="LogoContainer">
-                {(offsetY !== 0) ? <img className="LogoSlideSigil" style={{transform: `translatex(-${slideSlower}rem)`}}  src={Sigil} />
-                    : <img className="LogoSigil" src={Sigil} />
-                }
-                {/* <img className="LogoSigil" src={Sigil} /> */}
-                {(offsetY !== 0) ? <img className="LogoSlideSystems" style={{transform: `translatex(${slideSlower}rem)`}} src={SYSTEMS} /> :
-                    <img className="LogoSystems" src={SYSTEMS} />
-                }
-                {/* <img className="LogoSystems" src={SYSTEMS} /> */}
-            </div>
-            <FontAwesomeIcon className="Arrow" icon={faArrowDown} />
-        </div>
-    );
+            <p>If you're looking for a skilled engineer to bring your vision to life with innovative and effective solutions, <a href="#contact">let's connect</a>.</p>
+    </section>
+      </div>
+
+
+    </div>
+  );
 }

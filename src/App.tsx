5775b40c7740ed39a4d7f9b7642819b425f5b25a
notes <notes@kali.kali>
2023-11-15 22:20:53 -0600

    Added deploy-production script

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/styles.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
+
 function App() {
   return (
     <Router> {/* Wrap Routes inside Router */}

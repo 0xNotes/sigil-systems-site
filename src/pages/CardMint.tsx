825c9b2b472ff58666fba573f671551e778f7b98
notes <notes@sigil.systems>
2024-06-03 14:31:31 -0500

    Fixxed back image iteration

--- src/pages/CardMint.tsx
+++ src/pages/CardMint.tsx
@@ -1,20 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
 import FancyButton from "../components/FancyButton";
 
-
+// Generate image paths
+const imageCount = 7; // Update this count based on the number of images
+const images = Array.from({ length: imageCount }, (_, i) => `src/assets/cardBack/${i + 1}.png`);
 
 export default function CardMint() {
-    return (
-        <div className="cardMint">
-                <FancyButton />
-            <div>Each Bidness Card ($BID) will have randomized traits including a randomized discount for services rendered. (These do not stack)</div>
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const cardRef = useRef<HTMLDivElement>(null);
 
-            <div className="card-container">
-                <div className="card">
-                    <img className="front" src="src/assets/FrontCard.png"></img>
-                    <img className="back" src="src/assets/BackCard.png"></img>
-                </div>
-            </div>
+  useEffect(() => {
+    const handleAnimationIteration = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      // For randomization, use the following line instead:
+      // setCurrentIndex(Math.floor(Math.random() * images.length));
+    };
+
+    const cardElement = cardRef.current;
+    if (cardElement) {
+      cardElement.addEventListener("animationiteration", handleAnimationIteration);
+    }
+
+    return () => {
+      if (cardElement) {
+        cardElement.removeEventListener("animationiteration", handleAnimationIteration);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="cardMint">
+      <div className="card-container">
+        <div className="card" ref={cardRef}>
+          <img className="front" src="src/assets/cardFront/FRONT.png" />
+          <img className="back" src={images[currentIndex]} />
         </div>
+      </div>
 
-    );
+      <div>
+        <FancyButton />
+        {/* <div>Each Bidness Card ($BID) will have randomized traits including a randomized discount for services rendered. (These do not stack)</div> */}
+      </div>
+    </div>
+  );
 }
